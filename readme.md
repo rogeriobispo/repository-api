0a3a88931f2070687b99bfca72244ae68160355e
Rogerio Bispo <rogerio_pd@yahoo.com.br>
2020-04-27 22:13:48 -0300

    repository api done.

---
+++ readme.md
@@ -0,0 +1,29 @@
+## desafio rocketseat RocketSeat
+
+
+simple crud api repositóries.
+
+## rotas
+get("/repositories")
+- lista repostirórios
+
+post("/repositories");
+- cria repositório
+
+app.put("/repositories/:id");
+- atualiza repositório
+
+app.delete("/repositories/:id");
+- deleta repositório
+
+app.post("/repositories/:id/like");
+- likes no repositório
+
+
+## Tecnologias.
+- node
+- express
+- typescript
+- eslint
+- prettier
+
--- src/app.js
+++ src/app.js
@@ -1,33 +1,69 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (request, response) => response.json(repositories));
+
+app.post('/repositories', (request, response) => {
+  const id = uuid();
+  const { title, url, techs } = request.body;
+  const likes = 0;
+  const repostory = {
+    id, title, url, techs, likes,
+  };
+
+  repositories.push(repostory);
+  return response.json(repostory);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repositorieExists = repositories.find((repo) => repo.id === id);
+  if (!repositorieExists) response.status(400).json({ msg: 'repositorio não localizado' });
+  repositories.map((repo) => {
+    if (repo.id === id) {
+      if (title) repo.title = title;
+      if (url) repo.url = url;
+      if (techs) repo.techs = techs;
+    }
+  });
+
+  const repositorie = repositories.find((repo) => repo.id === id);
+
+  return response.status(200).json(repositorie);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const repositorieExists = repositories.find((repo) => repo.id === id);
+  if (!repositorieExists) response.status(400).json({ msg: 'repositorio não localizado' });
+  const repo = repositories.filter((repo) => repo.id !== id);
+  repositories = repo;
+  return response.status(204).json();
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+  const repositorieExists = repositories.find((repo) => repo.id === id);
+  if (!repositorieExists) response.status(400).json({ msg: 'repositorio não localizado' });
+  repositories.map((repo) => {
+    if (repo.id === id) {
+      repo.likes += 1;
+    }
+  });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repositorie = repositories.find((repo) => repo.id === id);
+
+  return response.status(200).json(repositorie);
 });
 
 module.exports = app;
